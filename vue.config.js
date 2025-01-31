ba9ec97886c4568dfd7c4fd9b33cad6b7c4014cc
o <3vefaaaa@mail.ru>
2025-02-01 06:20:39 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "/test-banki-shop/",
-});
+module.exports = {
+  filenameHashing: false,
+  configureWebpack: {
+    output: {
+      filename: 'js/[name].js', 
+      chunkFilename: 'js/[name].js'
+    }
+  },
+  publicPath: './',
+};
